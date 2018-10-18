67e5b4c726cc76cecb296c7f7ef190cddd719f45
Musa <musa@iMac10.local>
2018-10-18 15:09:10 -0400

    getting started

---
+++ unit_six_project.py
@@ -0,0 +1,10 @@
+def choose():
+    number = int(input("Choose any number, it must be greater than or equal to two"))
+    number>2
+    return True
+else:
+    return False
+
+
+
+listofnumbers = [2, 3, 4, 5, 6]
